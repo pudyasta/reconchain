4b27a99c3f5e347e1beb309f12dd3bcb61f4017a
pudyasta <pudyastasatria@gmail.com>
2022-10-07 14:06:20 +0700

    first page for tracking carbon

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./pages/**/*.{js,ts,jsx,tsx}",
+    "./components/**/*.{js,ts,jsx,tsx}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+        "dark-blue": "#080618",
+        "sea-blue": "#0D6EFD",
+      },
+    },
+  },
+  plugins: [],
+};
